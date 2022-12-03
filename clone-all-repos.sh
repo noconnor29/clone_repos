784ec1a073175068d1d17556f3261bc615ad63a8
Nick O'Connor <36704600+noconnor29@users.noreply.github.com>
2022-12-03 13:27:18 -0500

    Clone all repos for a user, with/out auth, to a specified location

---
+++ clone-all-repos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read -p "GitHub Username: " GITHUB_USER
+read -sp "GitHub API Token: " API_TOKEN
+read -p "Enter clone path. Default: PWD: " DIR
+DIR="${DIR:=.}"
+
+pushd $DIR
+
+REPOS=( $(curl -H GET -u $API_TOKEN:x-oauth-basic https://api.github.com/search/repositories?q=user:$GITHUB_USER | grep -E 'git@(.*).git' | awk '{print substr($2,2,length($2)-3); }') )
+
+for LINE in "${REPOS[@]}"
+do
+	git clone $LINE
+done
+
+popd
--- get-pull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# do a get request to https://api.github.com/users/noconnor29/repos
-# will also need a personal API token in bash_profile?
-repos=( $(curl -s https://api.github.com/users/noconnor29/repos | grep -E 'git@(.*).git' | awk '{print substr($2, 2, length($2)-3); }') )
-
-for line in "${repos[@]}"
-do
-	git clone $line
-done
