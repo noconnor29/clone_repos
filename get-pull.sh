c3d0a2673c74c93227834e3ba55f2c9534e539c0
Nick O'Connor <36704600+noconnor29@users.noreply.github.com>
2022-12-02 21:57:03 -0500

    working pull for public repos

--- get-pull.sh
+++ get-pull.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 # do a get request to https://api.github.com/users/noconnor29/repos
 # will also need a personal API token in bash_profile?
-# use field ssh_url": "git@github.com:noconnor29/ansible-role-harden.git"
-repos=( $(curl -s https://api.github.com/users/noconnor29/repos | jq '.[]|.html_url') )
+repos=( $(curl -s https://api.github.com/users/noconnor29/repos | grep -E 'git@(.*).git' | awk '{print substr($2, 2, length($2)-3); }') )
 
-#repos=(
-#	git@github.com:noconnor29/ansible-role-external.git
-#	git@github.com:noconnor29/ansible-role-ssh-audit.git
-#	git@github.com:noconnor29/ansible-role-github-deploy.git
-#	git@github.com:noconnor29/ansible-role-media.git
-#	git@github.com:noconnor29/ansible-role-bastion.git
-#	git@github.com:noconnor29/ansible.git
-#	git@github.com:noconnor29/.userconfigs.git
-#	git@github.com:noconnor29/.external-configs.git
-#	git@github.com:noconnor29/CodonAnalysis.git
-#	git@github.com:noconnor29/ansible-role-cloud-init.git
-#	git@github.com:noconnor29/ansible-role-harden.git
-#	git@github.com:noconnor29/ansible-role-ansiblesrv.git
-#	git@github.com:noconnor29/external-docker-compose.git
-#	git@github.com:noconnor29/internal-docker-compose.git
-#)
-#declare -p repos
 for line in "${repos[@]}"
-do 
-	git clone $line.git
+do
+	git clone $line
 done
